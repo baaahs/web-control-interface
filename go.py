57f438092e91b82a0da677887c0bbaa52fe5ae5d
Josh Durbin <durbinjo593@gmail.com>
2018-08-14 10:19:01 -0700

    Update go.py

--- go.py
+++ go.py
@@ -9,11 +9,6 @@ config = {
         'server.socket_host': '0.0.0.0',
         'server.socket_port': port,
         'server.thread_pool': 1
-    },
-    '/': {
-        'tools.staticdir.on': True,
-        'tools.staticdir.dir': '/home/baaahs/web_control/static',
-        'tools.staticdir.index': 'index.html'
     }
 }
 
